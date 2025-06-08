7b27dc327246b5c058932518f1e45b884b594761
Julia Canalle <julia.canalle4@gmail.com>
2025-06-08 12:10:58 -0300

    Commit inicial do projeto

---
+++ src/test/java/med/voll/bibliotecainfnetapi/BibliotecaInfnetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package med.voll.bibliotecainfnetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaInfnetApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
